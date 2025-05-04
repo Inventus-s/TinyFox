1d0c3840cb1f53442c1a87ddf4e5c99b4417366c
Mukund Kumar <123208378+Inventus-s@users.noreply.github.com>
2025-05-04 22:12:37 +0530

    getUser API setup

--- src/db/apiAuth.ts
+++ src/db/apiAuth.ts
@@ -10,3 +10,11 @@ export async function login({ email, password }: { email: string, password: stri
 
     return data;
 }
+
+export async function getCurrentUser() {
+    const { data: session, error } = await supabase.auth.getSession();
+    
+    if (!session.session) return null;
+    if (error) throw new Error(error.message);
+    return session.session?.user;
+}
