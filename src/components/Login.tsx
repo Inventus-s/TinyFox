3257afeb585307c86e581266452be5fb21b4f5f5
Mukund Kumar <123208378+Inventus-s@users.noreply.github.com>
2025-05-04 22:06:13 +0530

    Done with login setup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,10 +10,11 @@ import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import { BeatLoader } from "react-spinners";
 import Error from "./Error";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as Yup from 'yup'
 import useFetch from "@/hooks/useFetch";
 import { login } from "@/db/apiAuth";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 interface Errors {
     email?: string;
@@ -28,6 +29,10 @@ function Login() {
         password: "",
     })
 
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const longLink = searchParams.get('createNew');
+
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setFormData((prev) => ({
@@ -36,7 +41,13 @@ function Login() {
         }))
     };
 
-    const {data, error, loading} = useFetch(login, formData)
+    const { data, error, loading, fn: fnLogin } = useFetch(login, formData);
+
+    useEffect(() => {
+        if (error === null && data) {
+            navigate(`/dashboard?${longLink ? `createNew${longLink}` : ""}`)
+        }
+    }, [data, error])
 
     const handleLogin = async () => {
         setErrors({});
@@ -49,6 +60,7 @@ function Login() {
             });
             await schema.validate(formData, { abortEarly: false });
             // api call
+            await fnLogin()
         } catch (error: unknown) {
             const newErrors: Record<string, string> = {};
             if (error instanceof Yup.ValidationError) {
@@ -68,7 +80,7 @@ function Login() {
                 <CardHeader>
                     <CardTitle>Login</CardTitle>
                     <CardDescription>to your account if you already have one</CardDescription>
-                    {/* <Error message="Some Error" /> */}
+                    {error && <Error message={error?.message} />}
                 </CardHeader>
                 <CardContent className="space-y-2">
                     <div className="space-y-1">
@@ -82,7 +94,7 @@ function Login() {
                 </CardContent>
                 <CardFooter>
                     <Button onClick={handleLogin}>
-                        {true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+                        {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
                     </Button>
                 </CardFooter>
             </Card>
